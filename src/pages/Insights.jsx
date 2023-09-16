1cc10801011e76323ebd14e76a252eacfd8a9f9d
Matthew Houston <matthouston027@gmail.com>
2023-09-16 10:38:38 -0500

    Fix harsh lighting of themes and move chart section to Insights

--- src/pages/Insights.jsx
+++ src/pages/Insights.jsx
@@ -1,9 +1,30 @@
 import PageContent from "../components/PageContent"
+import Card from '../components/UI/Card'
+
+import { useDispatch, useSelector } from 'react-redux'
+import { uiActions } from "../components/store/ui-slice";
+import { weightActions } from "../components/store/weight-slice";
+
+import WeightEntry from "../components/Weight/WeightEntry";
 import WeightChart from "../components/Weight/WeightChart";
 
 const Insights = () => {
+    const dispatch = useDispatch();
+
+    const showModalHandler = (e) => {
+        dispatch(uiActions.showModal({
+            title: 'Entry',
+            message: null,
+            componentName: e.target.name
+        })
+        )
+    }
+
     return <PageContent title="Insights">
-        <WeightChart />
+        <WeightEntry onModal={showModalHandler} />
+        <Card>
+            <WeightChart />
+        </Card>
     </PageContent>
 }
 
--- src/pages/Workout.jsx
+++ src/pages/Workout.jsx
@@ -1,33 +1,12 @@
 import PageContent from "../components/PageContent"
-import Card from '../components/UI/Card'
-
-import { useDispatch, useSelector } from 'react-redux'
-import { uiActions } from "../components/store/ui-slice";
-import { weightActions } from "../components/store/weight-slice"
-
-import WeightEntry from "../components/Weight/WeightEntry";
-import WeightChart from "../components/Weight/WeightChart";
+import Card from "../components/UI/Card";
 
 const Workout = () => {
 
-    const dispatch = useDispatch();
-
-    const showModalHandler = (e) => {
-        dispatch(uiActions.showModal({
-            title: 'Entry',
-            message: null,
-            componentName: e.target.name
-        })
-        )
-    }
-
-
-    return <PageContent title="Chart">
-        <WeightEntry onModal={showModalHandler} />
+    return <PageContent title="Workout">
         <Card>
-            <WeightChart />
+            Add a Workout to get started!
         </Card>
-
     </PageContent>
 }
 

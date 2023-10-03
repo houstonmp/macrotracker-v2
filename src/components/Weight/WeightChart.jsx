83d65c68c4c5f1f0d033a33ed3ae15b2e4318247
Matthew Houston <matthouston027@gmail.com>
2023-10-02 23:06:22 -0500

    Add some styling to the weight chart to show acceptable ranged for BMI.

--- src/components/Weight/WeightChart.jsx
+++ src/components/Weight/WeightChart.jsx
@@ -1,11 +1,13 @@
 import 'chart.js/auto';
 import { Chart } from 'react-chartjs-2';
-import { Filler } from 'chart.js'
+import { Chart as ChartJS, Filler } from 'chart.js'
 import Card from '../UI/Card';
 import classes from "./WeightEntry.module.css"
 
 import { useSelector } from "react-redux";
 import { BMIBoundaries } from '../../assets/functions';
+
+ChartJS.register(Filler);
 
 const WeightChart = (props) => {
     var style = getComputedStyle(document.body);
@@ -25,92 +27,100 @@ const WeightChart = (props) => {
 
     let alphaColLight = colorPrimaryBlue900;
     let alphaColDark = colorPrimaryBlue;
-    let gammaCol3 = colorPrimaryBlue;
-    let gammaCol2 = colorPrimaryBlue200;
-    let gammaCol1 = colorPrimaryBlue400;
+    // let gammaCol3 = colorPrimaryBlue;
+    // let gammaCol2 = colorPrimaryBlue200;
+    // let gammaCol1 = colorPrimaryBlue400;
     let gammaCol = colorPrimaryBlue600;
 
 
     if (theme === 'dark') {
         alphaColDark = colorPrimaryBlue;
-        gammaCol3 = colorPrimaryBlue600
-        gammaCol2 = colorPrimaryBlue400;
-        gammaCol1 = colorPrimaryBlue200;
         gammaCol = colorPrimaryBlue;
     }
+    let gammaCol3 = colorPrimaryBlue600
+    let gammaCol2 = colorPrimaryBlue400;
+    let gammaCol1 = colorPrimaryBlue200;
 
-    console.log(user)
+    // }
 
     const weightObj = useSelector(state => state.weight.weightObj);
-    // ChartJS.register(Filler)
 
     return <Card classes={props.classes}>
-        <div className={classes.backgroundColor}>
-            <Chart type='line' options={{
-                responsive: true,
-                maintainAspectRatio: true,
-                color: alphaColDark,
-                plugins: {
-                    tooltip: true,
-                    filler: {
-                        backgroundColor: colorPrimaryBlue,
-                    }
-                    // customCanvasBackgroundColor: {
-                    //     color: colorPrimaryBlue800,
-                    //     fill: true
-                    // }
+        <Chart type='line' options={{
+
+            maintainAspectRatio: true,
+
+            color: alphaColDark,
+            plugins: {
+                tooltip: true,
+                filler: {
+                    backgroundColor: colorPrimaryBlue,
                 },
-                scales: {
-                    x: {
-                        grid: {
-                            color: theme === 'light' ? alphaColLight : alphaColDark
-                        },
-                        ticks: {
-                            color: theme === 'light' ? alphaColLight : alphaColDark
-                        }
+                legend: {
+                    display: false
+                },
+                responsive: true,
+            },
+            scales: {
+                x: {
+                    grid: {
+                        color: theme === 'light' ? alphaColLight : alphaColDark
                     },
-                    y: {
-                        grid: {
-                            color: theme === 'light' ? alphaColLight : alphaColDark
-                        },
-                        ticks: {
-                            color: theme === 'light' ? alphaColLight : alphaColDark
-                        }
+                    ticks: {
+                        color: theme === 'light' ? alphaColLight : alphaColDark
+                    }
+                },
+                y: {
+                    grid: {
+                        color: theme === 'light' ? alphaColLight : alphaColDark
+                    },
+                    ticks: {
+                        color: theme === 'light' ? alphaColLight : alphaColDark
                     }
                 }
             }
-            }
-                data={{
-                    labels: weightObj.map((el) => el.date),
-                    datasets: [
+        }
+        }
+            data={{
+                labels: weightObj.map((el) => el.date),
+                datasets: [{
+                    id: 1,
+                    label: 'Weight',
+                    data: weightObj.map((weight) => weight.lbs),
+                    borderColor: gammaCol
+                },
 
-                        {
-                            id: 2,
-                            label: 'BMI 18.5',
-                            data: user.height && weightObj.map((weight) => BMIBoundaries(18.5, user.height.in)),
-                            borderColor: gammaCol1,
+                {
+                    id: 2,
+                    label: 'BMI 18.5',
+                    data: user.height && weightObj.map((weight) => BMIBoundaries(18.5, user.height.in)),
+                    borderColor: gammaCol2,
+                    backgroundColor: '#ff6384',
+                    fill: true
 
-                        },
-                        {
-                            id: 3,
-                            label: 'BMI 25',
-                            data: user.height && weightObj.map((weight) => BMIBoundaries(25, user.height.in)),
-                            borderColor: gammaCol2,
-                        },
-                        {
-                            id: 4,
-                            label: 'BMI 30',
-                            data: user.height && weightObj.map((weight) => BMIBoundaries(30, user.height.in)),
-                            borderColor: gammaCol3,
-                        }, {
-                            id: 1,
-                            label: 'Weight',
-                            data: weightObj.map((weight) => weight.lbs),
-                            borderColor: gammaCol
-                        },
-                    ]
-                }} />
-        </div>
+                },
+                {
+                    id: 3,
+                    label: 'BMI 25',
+                    data: user.height && weightObj.map((weight) => BMIBoundaries(25, user.height.in)),
+                    borderColor: gammaCol2,
+                    backgroundColor: '#4bc0c0',
+                    fill: true
+                },
+                {
+                    id: 4,
+                    label: 'BMI 30',
+                    data: user.height && weightObj.map((weight) => BMIBoundaries(30, user.height.in)),
+                    borderColor: gammaCol2,
+                    fill: {
+                        target: 'origin',
+                        // above: '#ff6384',
+                        below: '#ff9f40',
+                    },
+                    backgroundColor: '#ff9f40',
+                },
+                ]
+            }} />
     </Card>
 
 }
